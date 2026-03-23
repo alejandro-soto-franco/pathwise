a71116cbd52b6a58c6d54c7aa34e0af9d244db29
alejandrosotofranco <soto.alejandro2025@gmail.com>
2026-03-23 16:27:44 +0300

    feat(py): add PyO3 module skeleton with exception types

--- pathwise-py/src/lib.rs
+++ pathwise-py/src/lib.rs
@@ -1,6 +1,10 @@
 use pyo3::prelude::*;
 
+mod py_error;
+
 #[pymodule]
-fn _pathwise(_py: Python<'_>, _m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn _pathwise(py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add("NumericalDivergence", py.get_type::<py_error::NumericalDivergence>())?;
+    m.add("ConvergenceError", py.get_type::<py_error::ConvergenceError>())?;
     Ok(())
 }
---
+++ python/pathwise/__init__.py
@@ -0,0 +1,4 @@
+from pathwise._pathwise import NumericalDivergence, ConvergenceError
+
+__version__ = "0.1.0"
+__all__ = ["NumericalDivergence", "ConvergenceError"]
