d0171a2d1c89045bd21ea4a2b02fb888039cec32
alejandrosotofranco <soto.alejandro2025@gmail.com>
2026-03-23 23:18:34 +0300

    fix(py): use get_type_bound to silence clippy deprecation warning

--- pathwise-py/src/lib.rs
+++ pathwise-py/src/lib.rs
@@ -5,14 +5,20 @@ mod py_process;
 mod py_scheme;
 mod py_simulate;
 
-use py_process::{PySDE, bm, gbm, ou, sde};
-use py_scheme::{PyEuler, PyMilstein, euler, milstein};
+use py_process::{bm, gbm, ou, sde, PySDE};
+use py_scheme::{euler, milstein, PyEuler, PyMilstein};
 use py_simulate::simulate;
 
 #[pymodule]
 fn _pathwise(py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add("NumericalDivergence", py.get_type::<py_error::NumericalDivergence>())?;
-    m.add("ConvergenceError", py.get_type::<py_error::ConvergenceError>())?;
+    m.add(
+        "NumericalDivergence",
+        py.get_type_bound::<py_error::NumericalDivergence>(),
+    )?;
+    m.add(
+        "ConvergenceError",
+        py.get_type_bound::<py_error::ConvergenceError>(),
+    )?;
     m.add_class::<PySDE>()?;
     m.add_function(wrap_pyfunction!(bm, m)?)?;
     m.add_function(wrap_pyfunction!(gbm, m)?)?;
